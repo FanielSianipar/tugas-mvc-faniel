c307b157fef79f5515c3dd95204322a60c45ca08
FanielSianipar <fanielsianipar01@gmail.com>
2023-04-03 21:23:37 +0700

    delete route ' / '

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
 Route::get('/user', [UserController::class, 'index']);
